32a55b0ab3a048e339400f01be2b996b1a9b5bfd
MarioBlasi <mblasi509@gmail.com>
2023-06-06 13:36:49 +0100

    admin and dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,56 +1,28 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.admin')
 
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+@section('content')
+<div class="container-fluid">
+    <h2 class="fs-4 text-secondary my-4 bg-danger">
+        {{ __('Dashboard') }}
+    </h2>
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">{{ __('User Dashboard') }}</div>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-
-    <body >
-        <div id="app">
-            <nav class="navbar navbar-expand-md navbar-light bg-light">
-                  <div class="container">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="collapsibleNavId">
-                        <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                                <div class="dropdown-menu" aria-labelledby="dropdownId">
-                                    <a class="dropdown-item" href="#">Action 1</a>
-                                    <a class="dropdown-item" href="#">Action 2</a>
-                                </div>
-                            </li>
-                        </ul>
-                        <form class="d-flex my-2 my-lg-0">
-                            <input class="form-control me-sm-2" type="text" placeholder="Search">
-                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                        </form>
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
                     </div>
-              </div>
-            </nav>
-            
+                    @endif
 
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
         </div>
-    </body>
-</html>
+    </div>
+</div>
+@endsection
+
 
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -16,8 +16,25 @@
     </head>
 <body>
     <div id="app">
+
+        <nav class="navbar navbar-dark stick-top bg-dark flex-md-nowrap p-0 shadow">
+            <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3">BOOLPRESS</a>
+            <button class="navbar-toggler position-absolute d-md-none collapsed" type="button"
+            data-target="#sidebarMenu" data-toggle="collapse" aria-controls="sidebarMenu" aria-expanded="false"
+             aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+            <ul class="navbar-nav px-3">
+                <li class="nav-item text-nowrap">
+                    <a class="nav-link" href="#">Sign out</a>
+                </li>
+            </ul>
+        </nav>
+
         <nav class="navbar navbar-expand-md navbar-light bg-light">
-              <div class="container">
+              <div class="container-fluid">
                 <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">Navbar</a>
 
                 <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
@@ -71,14 +88,65 @@
                         @endguest
                     </ul>
                 </div>
-        </nav>
+        </nav> 
         
-        <main class="">
-            @yield('contant')
-            
-        </main>
+   
+        <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+            @yield('content')   
+            {{-- <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 class="h3">Dashboard</h1>
+                <div class="btn-toolbar mb-2 mb-md-0">
+                    <div class="btn-group mr-2">
+                        <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle"></button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle"></button>
+                    </div>
+                    <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
+                        <span data-feather="calendar"></span>
+                        this week
+                    </button>
+                </div>
+            </div>
+     
 
+        <canvas class="my-4 w-100" id="myChart" width="800" height="200"></canvas> --}}
+
+        {{-- <h1>title</h1>
+        <table class="table table-striped table-sm"> --}}
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
+
+        </main>  
     </div>
 </body>
-</html>    
+</html>   
+
+b5
+
     
